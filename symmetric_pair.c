7f2fa9559d89b7d6b1bd3a3ca6af12216a4a8527
jafar9 <jafarbasha39@gmail.com>
2017-06-20 10:08:42 +0530

    Update symmetric_pair.c

--- symmetric_pair.c
+++ symmetric_pair.c
@@ -1,34 +1,45 @@
+/*
+sample input:
+6
+11 20
+30 40
+30 100
+5 10
+40 30
+10 5
+sample output:
+
+30->40
+5->10
+
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 void main()
 {
-	int n,a[1000][2],k,i,j,z;
+	int n,a[1000][2],i,j;
 	int hash[100000];
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
 	{
-		for(j=0;j<2;j++)
-		{
-			scanf("%d",&a[i][j]);
-		}
+	   for(j=0;j<2;j++)
+	    {
+	       scanf("%d",&a[i][j]);
+	    }
 	}
 	for(i=0;i<n;i++)
 	{
-		int first=a[i][0]; //10 5
-		int sec=a[i][1]; // 5 10
-		if(hash[first]==sec) // hash[5]=10
-		{
-			printf("%d->%d\n",sec,first);
-		}
-		else
-		{
-			hash[sec]=first; //hash[5]=10
-
-		}
-	}
-
-	
-
-
+	   int first=a[i][0]; 
+	   int sec=a[i][1]; 
+	   if(hash[first]==sec) 
+	    {
+	      printf("%d->%d\n",sec,first);
+	    }
+	   else
+	    {
+	      hash[sec]=first; 
+	    }
+	 }
 }
